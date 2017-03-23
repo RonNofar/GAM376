5057bf301bdf666321faa380b4b4038f5adda3a0
kodomazer <brianzha@gmail.com>
2017-03-22 21:49:22 -0700

    Enemy Spawning
    Spawns a random enemy of any type.
    
    TODO: Create selection masks for what kinds of enemies spawn

--- Assets/Scripts/Enemy/EnemySpawnerHandler.cs
+++ Assets/Scripts/Enemy/EnemySpawnerHandler.cs
@@ -12,17 +12,33 @@ namespace KRaB.Split.Enemy
         [SerializeField]
         private RTool.FloatRange enemySpawnDelay;
         [SerializeField]
-        private GameObject[] enemyList;
+        private GameObject enemy;
         [SerializeField]
         private ColorManager.eColors colorType;
 
+        private bool spawning;
+
         // Use this for initialization
         void Start() {
-
+            Time.timeScale = 1;
+            spawning = true;
+            StartCoroutine(spawnEnemies());
         }
 
         void FixedUpdate() {
+        }
+        private IEnumerator spawnEnemies()
+        {
+            Debug.Log("Spawn start");
+            while (spawning)
+            {
+                Debug.Log("Spawn Continues");
+                enemy.GetComponent<SlimeHandler>().Color = (UI.ColorManager.eColors)Random.Range(1, 8);
+                GameObject temp = Instantiate(enemy);
+                temp.transform.position = transform.position;
+                yield return new WaitForSeconds(1);
 
+            }
         }
     }
 }
