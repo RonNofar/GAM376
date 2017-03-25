23b1f13a67beb4e74ea5f116c44cbec89c7a863a
kodomazer <brianzha@gmail.com>
2017-03-25 04:56:35 -0700

    File creation
    Basic file creation and class hierarchy made
    
    Entity:
     - Will contain base movement routines (possibly also the new parent of player control
    
    Enemy:
     - Possibly have basic targeting methods
     - Allow for a common interface for enemies to interact with the AIManager
     - (Might just be a place for the shovel tossing stuff to be placed, so non-slime objects can be tossed with the shovel)
    
    Slime:
     - To replace current slimeHandler
     - handle slime coloring
     - possibly have a child class for larger slimes

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KRaB.Split.Enemy
+{
+    public abstract class Enemy : Entity
+    {
+
+        // Use this for initialization
+        protected override void Start()
+        {
+            base.Start();
+
+        }
+
+        // Update is called once per frame
+        protected override void Update()
+        {
+            base.Update();
+
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/Slime.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KRaB.Split.Enemy
+{
+    public class Slime : Enemy
+    {
+
+
+        // Use this for initialization
+        protected override void Start()
+        {
+            base.Start();
+
+        }
+
+        // Update is called once per frame
+        protected override void Update()
+        {
+            base.Update();
+
+        }
+    }
+}
