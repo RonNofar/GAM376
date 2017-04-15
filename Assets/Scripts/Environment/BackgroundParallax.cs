6c0dec85bc6a027af0caeb0e8fac46c5604a7316
kodomazer <brianzha@gmail.com>
2017-04-14 17:44:07 -0700

    Cherrypicked BackgroundParallax
    Just added the files and not the scene.

---
+++ Assets/Scripts/Environment/BackgroundParallax.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KRaB.Environment{
+    public class BackgroundParallax : MonoBehaviour
+    {
+
+        public Transform camPosition;
+
+        public Vector3 offset;
+
+
+        public float xRatio;
+        public float yRatio;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 cPos = camPosition.position;
+            cPos += offset;
+            Vector3 oPos = new Vector3();
+
+            oPos.x = cPos.x * xRatio;
+            oPos.y = cPos.y * yRatio;
+            transform.position = oPos;
+        }
+    }
+}
